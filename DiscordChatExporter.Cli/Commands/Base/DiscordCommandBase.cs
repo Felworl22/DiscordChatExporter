090e6672e68538c3f37ba4bbdcfc421c584634b9
Felworl22 <135156503+Felworl22@users.noreply.github.com>
2023-12-11 05:51:20 +0000

    update file

---
+++ DiscordChatExporter.Cli/Commands/Base/DiscordCommandBase.cs
@@ -0,0 +1,64 @@
+using System;
+using System.Threading.Tasks;
+using CliFx;
+using CliFx.Attributes;
+using CliFx.Infrastructure;
+using DiscordChatExporter.Core.Discord;
+
+namespace DiscordChatExporter.Cli.Commands.Base;
+
+public abstract class DiscordCommandBase : ICommand
+{
+    [CommandOption(
+        "token",
+        't',
+        EnvironmentVariable = "DISCORD_TOKEN",
+        Description = "Authentication token."
+    )]
+    public required string Token { get; init; }
+
+    [Obsolete("This option doesn't do anything. Kept for backwards compatibility.")]
+    [CommandOption(
+        "bot",
+        'b',
+        EnvironmentVariable = "DISCORD_TOKEN_BOT",
+        Description = "This option doesn't do anything. Kept for backwards compatibility."
+    )]
+    public bool IsBotToken { get; init; } = false;
+
+    private DiscordClient? _discordClient;
+    protected DiscordClient Discord => _discordClient ??= new DiscordClient(Token);
+
+    public virtual ValueTask ExecuteAsync(IConsole console)
+    {
+#pragma warning disable CS0618
+        // Warn if the bot option is used
+        if (IsBotToken)
+        {
+            using (console.WithForegroundColor(ConsoleColor.DarkYellow))
+            {
+                console
+                    .Error
+                    .WriteLine(
+                        "Warning: Option --bot is deprecated and should not be used. "
+                            + "The type of the provided token is now inferred automatically. "
+                            + "Please update your workflows as this option may be completely removed in a future version."
+                    );
+            }
+        }
+#pragma warning restore CS0618
+
+        // Note about interactivity
+        if (console.IsOutputRedirected)
+        {
+            console
+                .Output
+                .WriteLine(
+                    "Note: Output streams are redirected, rich console interactions are disabled. "
+                        + "If you are running this command in Docker, consider allocating a pseudo-terminal for better user experience (docker run -it ...)."
+                );
+        }
+
+        return default;
+    }
+}
